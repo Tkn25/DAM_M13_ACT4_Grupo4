d2ccb184fc2bcaed2db17b978d85010c19b12012
Komandarm-Knuckles <122950778+Komandarm-Knuckles@users.noreply.github.com>
2024-05-10 05:33:29 +0200

    Código de LoginSeleccion.java que faltaba

--- app/src/main/java/com/example/dam_m13_act4_grupo4/Login/LoginSeleccion.java
+++ app/src/main/java/com/example/dam_m13_act4_grupo4/Login/LoginSeleccion.java
@@ -1,6 +1,9 @@
 package com.example.dam_m13_act4_grupo4.Login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.dam_m13_act4_grupo4.Cliente.MascotasCliente;
+import com.example.dam_m13_act4_grupo4.Cliente.PrincipalCliente;
 import com.example.dam_m13_act4_grupo4.R;
 
 public class LoginSeleccion extends AppCompatActivity {
 
+    private ImageButton cliente, veterinario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +27,24 @@ public class LoginSeleccion extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        cliente = findViewById(R.id.imageButton);
+        veterinario = findViewById(R.id.imageButton2);
+        cliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginSeleccion.this, LoginCliente.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        veterinario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginSeleccion.this, LoginVeterinario.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
