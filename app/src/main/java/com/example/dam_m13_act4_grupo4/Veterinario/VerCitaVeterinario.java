99346ca0a9a0c773a996fb6abe9cd8a43c709a31
Komandarm-Knuckles <122950778+Komandarm-Knuckles@users.noreply.github.com>
2024-05-18 12:17:09 +0200

    Añadida actividad VerCitaVeterinario

---
+++ app/src/main/java/com/example/dam_m13_act4_grupo4/Veterinario/VerCitaVeterinario.java
@@ -0,0 +1,37 @@
+package com.example.dam_m13_act4_grupo4.Veterinario;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.dam_m13_act4_grupo4.R;
+
+public class VerCitaVeterinario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ver_cita_veterinario);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+        if (intent != null)
+        {
+            int idConsulta = intent.getIntExtra("idConsulta", 0);
+            String nombre = intent.getStringExtra("nombre");
+            String motivo = intent.getStringExtra("motivo");
+            String fecha = intent.getStringExtra("fecha");
+        }
+    }
+}
