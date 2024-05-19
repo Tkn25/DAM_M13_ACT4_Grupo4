868dd61a5cab7004cea3f9692586a02ef5e80714
svetlana <svetikpiciu@gmail.com>
2024-05-19 21:14:18 +0200

    Limpieda y docu de mascotaVeterinario

--- app/src/main/java/com/example/dam_m13_act4_grupo4/Veterinario/MascotasVeterinario.java
+++ app/src/main/java/com/example/dam_m13_act4_grupo4/Veterinario/MascotasVeterinario.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
@@ -27,9 +26,6 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.dam_m13_act4_grupo4.Cliente.DatosMascotaCliente;
-import com.example.dam_m13_act4_grupo4.Cliente.MascotasCliente;
-import com.example.dam_m13_act4_grupo4.Cliente.PrincipalCliente;
 import com.example.dam_m13_act4_grupo4.POJO.Mascota;
 import com.example.dam_m13_act4_grupo4.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -53,6 +49,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 public class MascotasVeterinario extends AppCompatActivity {
+    // Declaramos variables globales.
     private RecyclerView recycler;
     private final ArrayList<Mascota> mascotas = new ArrayList<>();
 
@@ -60,12 +57,14 @@ public class MascotasVeterinario extends AppCompatActivity {
     private ImageButton volver;
     private Spinner spinnerFiltro;
     private SearchView searchView;
-
-    private int size;
+    FloatingActionButton fab;
+    //Variable que se usa para guardar el ultimo Id para tener una referencia al insertar una nueva mascota.
+    private int ultimoId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Inicializar layout
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mascotas_veterinario);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -73,15 +72,21 @@ public class MascotasVeterinario extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        //Vinculamos los elementos de la vista con objetos Java
+        fab = findViewById(R.id.floatingActionButton);
+        volver = findViewById(R.id.imageButton6);
+        recycler = findViewById(R.id.recycler);
+        searchView = findViewById(R.id.searchView);
+        spinnerFiltro = findViewById(R.id.spinnerFiltro);
+        //Evento del boton flotante al hacer click sobre el
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Acción a realizar cuando se hace clic en el botón flotante
+                //Metodo que abre una nueva vista para añadir una mascota
                 añadir();
             }
         });
-        volver = findViewById(R.id.imageButton6);
+        //Metodo que al hacer click sobre el boton vuelve a la pantalla anterior
         volver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -90,23 +95,21 @@ public class MascotasVeterinario extends AppCompatActivity {
                 finish();
             }
         });
-        recycler = findViewById(R.id.recycler); // Inicializar RecyclerView
-        recycler.setLayoutManager(new LinearLayoutManager(this)); // Establec
+        //Inicializa la lista para ver los datos de cada mascota en el layout
+        recycler.setLayoutManager(new LinearLayoutManager(this));
         // Instancia y ejecuta la tarea asíncrona para obtener las mascotas
         ObtenerMascotasTask obtenerMascotasTask = new ObtenerMascotasTask();
         obtenerMascotasTask.execute();
-        // Inicializar SearchView y Spinner
-        searchView = findViewById(R.id.searchView);
-        spinnerFiltro = findViewById(R.id.spinnerFiltro);
+        //Adaptador que recoge los datos de opciones filtro y los añade al espinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.opciones_filtro, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerFiltro.setAdapter(adapter);
-        // Configurar escucha del SearchView
+
+        //Metodo de escucha search view 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                // No se utiliza
                 return false;
             }
 
@@ -133,13 +136,14 @@ public class MascotasVeterinario extends AppCompatActivity {
         return null;
     }
 
+    //Clase para la obtencion de datos de la mascota
     private class ObtenerMascotasTask extends AsyncTask<Void, Void, ArrayList<Mascota>> {
         ArrayList<Mascota> mascotasList = new ArrayList<>();
 
         @Override
         protected ArrayList<Mascota> doInBackground(Void... voids) {
-            String url = "http://192.168.0.14/ControlPaw/mascotasVeterinario.php"; // Sustituye por tu IPv4
-
+            String url = "http://192.168.0.14/ControlPaw/mascotasVeterinario.php";
+            //Conexion a php
             try {
                 URL direccion = new URL(url);
                 HttpURLConnection conexion = (HttpURLConnection) direccion.openConnection();
@@ -148,6 +152,7 @@ public class MascotasVeterinario extends AppCompatActivity {
 
                 InputStream entrada = conexion.getInputStream();
                 BufferedReader lector = new BufferedReader(new InputStreamReader(entrada));
+                //Obtemos la informacion
                 StringBuilder respuesta = new StringBuilder();
                 String linea;
 
@@ -158,7 +163,7 @@ public class MascotasVeterinario extends AppCompatActivity {
                 Document document = convertirStringToXMLDocument(respuesta.toString());
 
                 NodeList listaMascotas = document.getElementsByTagName("mascota");
-
+                //Mapear informacion
                 for (int i = 0; i < listaMascotas.getLength(); i++) {
                     Element element = (Element) listaMascotas.item(i);
                     int id = Integer.parseInt(element.getElementsByTagName("idMascota").item(0).getTextContent());
@@ -192,7 +197,8 @@ public class MascotasVeterinario extends AppCompatActivity {
                 e.printStackTrace();
 
             }
-            size = mascotasList.get(mascotasList.size() - 1).getId();
+            //Buscamos el ultimo registro y nos guardamos su ID para posteriormente insertar una nueva mascota y ponerle el ultimo ID +1
+            ultimoId = mascotasList.get(mascotasList.size() - 1).getId();
             return mascotasList;
         }
 
@@ -205,19 +211,18 @@ public class MascotasVeterinario extends AppCompatActivity {
                 mascotas.clear();
 
                 mascotas.addAll(mascotasList);
-
-                adaptador = new AdaptadorMain(mascotas); // Inicializar el adaptador con la lista de mascotas
-                recycler.setAdapter(adaptador); // Asignar el adaptador al RecyclerView
-
+                // Inicializar el adaptador con la lista de mascotas
+                adaptador = new AdaptadorMain(mascotas);
+                // Asignar el adaptador al RecyclerView
+                recycler.setAdapter(adaptador);
             } else {
-
                 Toast.makeText(getApplicationContext(),
                         "No se ha podido establecer la conexión. Por favor, inténtelo de nuevo más tarde.",
                         Toast.LENGTH_SHORT).show();
             }
         }
     }
-
+//Adaptador para visualizacion de los datosde la mascota
     private class AdaptadorMain extends RecyclerView.Adapter<AdaptadorMain.ViewHolder> implements Filterable {
         private ArrayList<Mascota> mascotas;
 
@@ -227,7 +232,7 @@ public class MascotasVeterinario extends AppCompatActivity {
         private String criterioFiltro;
 
         public class ViewHolder extends RecyclerView.ViewHolder {
-
+//Creamos el diseño del item de la lista
             private final TextView nombre;
             private final TextView especie;
             private final TextView peso;
@@ -246,12 +251,13 @@ public class MascotasVeterinario extends AppCompatActivity {
 
         public AdaptadorMain(ArrayList<Mascota> mascotas) {
             this.mascotas = mascotas;
+            // Copia de la lista de mascotas
             this.allMascota = mascotas;
-            this.mascotasFiltradas = new ArrayList<>(mascotas); // Inicializar lista filtrada con todos los datos
-            this.criterioFiltro = "nombre"; // Criterio de filtrado predeterminado
-            // nuevo codigo
-        }
+            this.mascotasFiltradas = new ArrayList<>(mascotas);
+            this.criterioFiltro = "nombre";
 
+        }
+// Filtro de la lista de mascotas
         @Override
         public Filter getFilter() {
             return new Filter() {
@@ -260,18 +266,20 @@ public class MascotasVeterinario extends AppCompatActivity {
                     String filtro = charSequence.toString().toLowerCase().trim();
                     ArrayList<Mascota> listaFiltrada = new ArrayList<>();
 
-                    // Implementa el filtrado para todos los criterios necesarios
+                   // Cada vez que se filtra lo hacemos desde de la copia que tiene todos los datos de todas las mascotas
                     String palabra = "";
                     mascotas = allMascota;
+                    // Si el search view esta vacio añadimos todas las mascotas
                     if (filtro.equals("")) {
                         listaFiltrada = mascotas;
                     } else {
+                        // Recorremos todas las listas de mascotas
                         for (Mascota mascota : mascotas) {
-                            palabra = mascota.getNombre().toLowerCase();
-                            if (criterioFiltro.equals("nombre") && palabra.contains(filtro.toLowerCase())) {
+                            // Buscamos que palabra del espinner hay puesta y obtenemos el texto introducido del search view
+                            palabra = (criterioFiltro.equals("nombre")) ? mascota.getNombre().toLowerCase() : mascota.getDni().toLowerCase();
+                            // Si el texto coincide con el dato de la mascota nos lo guardamos para enseñar la lista filtrada
+                            if (palabra.contains(filtro.toLowerCase())) {
                                 listaFiltrada.add(mascota);
-                            } else if (criterioFiltro.equals("DNI")) {
-                                palabra = mascota.getDni().toLowerCase();
                             }
                         }
                     }
@@ -284,14 +292,15 @@ public class MascotasVeterinario extends AppCompatActivity {
                 @Override
                 protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
                     mascotasFiltradas = (ArrayList<Mascota>) filterResults.values;
-                    notifyDataSetChanged(); // Notificar al adaptador sobre los cambios en la lista filtrada
+                    // Notificar al adaptador sobre los cambios en la lista filtrada
+                    notifyDataSetChanged();
                 }
             };
         }
 
-        public void setCriterioFiltro(String criterioFiltro) {
+       /* public void setCriterioFiltro(String criterioFiltro) {
             this.criterioFiltro = criterioFiltro;
-        }
+        }*/
 
         @NonNull
         @Override
@@ -319,7 +328,7 @@ public class MascotasVeterinario extends AppCompatActivity {
             } else if (mascota.getIdEspecie() == 4) {
                 holder.especie.setText("Hamster");
             }
-
+            // Al pulsar sobre cualquier item enviaremos los datos del item a una nueva vista para verlo en detalle
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -366,10 +375,12 @@ public class MascotasVeterinario extends AppCompatActivity {
     }
 
     private void añadir() {
+// Abre una nueva vista para insertar una nueva mascota
         Intent intent = new Intent(this, DatosMascotaVet.class);
+        // Le pasamos el dato insertar mascota como parametro para saber en la otra vista que hemos accedido para insertar
         intent.putExtra("clave", "insertarMascota");
 
-        intent.putExtra("idMascota", size + 1);
+        intent.putExtra("idMascota", ultimoId + 1);
         startActivity(intent);
         finish();
     }
