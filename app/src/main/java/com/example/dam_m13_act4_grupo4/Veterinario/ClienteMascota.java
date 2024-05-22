53cbda64c7bd627b2dc01161849af884b0b74380
svetlana <svetikpiciu@gmail.com>
2024-05-22 18:07:19 +0200

    Limpieza y docu de mascotaVeterinario

--- app/src/main/java/com/example/dam_m13_act4_grupo4/Veterinario/ClienteMascota.java
+++ app/src/main/java/com/example/dam_m13_act4_grupo4/Veterinario/ClienteMascota.java
@@ -45,6 +45,7 @@ public class ClienteMascota extends AppCompatActivity {
     private RecyclerView recycler;
     private final ArrayList<Dueno> duenos = new ArrayList<>();
     private AdaptadorMain adaptador;
+    private  ImageButton volver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,14 +56,12 @@ public class ClienteMascota extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        ImageButton volver = findViewById(R.id.imageButtonVolver);
+        volver = findViewById(R.id.imageButtonVolver);
         recycler = findViewById(R.id.recycler);
         //Creamos un objeto de tipo adaptador y lo asignamos a la recycler
         adaptador = new AdaptadorMain(duenos);
         recycler.setAdapter(adaptador);
         recycler.setLayoutManager(new LinearLayoutManager(this));
-
         volver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,19 +85,17 @@ public class ClienteMascota extends AppCompatActivity {
         }
         return null;
     }
+    // Creamos la conexion con la lista
     private class AdaptadorMain extends RecyclerView.Adapter<AdaptadorMain.ViewHolder> {
         private final ArrayList<Dueno> duenos;
-
         public class ViewHolder extends RecyclerView.ViewHolder {
             //Creamos la variables con los elementos del layout de cada item
-
             private final TextView tipo;
             private final TextView usuario;
             private final TextView DNI;
             private final TextView nombre;
             private final TextView telefono;
             private final TextView direccion;
-
 
             public ViewHolder(View view) {
                 super(view);
@@ -111,11 +108,9 @@ public class ClienteMascota extends AppCompatActivity {
                 direccion = view.findViewById(R.id.textViewDirItem);
             }
         }
-
         public AdaptadorMain(ArrayList<Dueno> duenos) {
             this.duenos = duenos;
         }
-
         @NonNull
         @Override
         public AdaptadorMain.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
@@ -123,44 +118,30 @@ public class ClienteMascota extends AppCompatActivity {
             View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.items_layout_cliente, viewGroup, false);
             return new ViewHolder(view);
         }
-
         @Override
         public void onBindViewHolder(@NonNull AdaptadorMain.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
-            //Asignamos el texto con el valor de las mascotas a los campos
+            //Rellenamos los elementos con los datos de los clientes
             Dueno dueno = duenos.get(position);
             holder.usuario.setText(String.valueOf(dueno.getUsuario()));
             holder.DNI.setText(String.valueOf(dueno.getDNI()));
             holder.nombre.setText(String.valueOf(dueno.getNombre()));
             holder.telefono.setText(String.valueOf(dueno.getTelefono()));
             holder.direccion.setText(String.valueOf(dueno.getDireccion()));
-
-            //Según la ID de la especie se mostrará un nombre u otro
+// Si el tipo es 1 le asignamos veterinario si es 2 le asignamos clinte
             if (dueno.getTipo() == 1) {
                 holder.tipo.setText("Veterinario");
             } else if (dueno.getTipo() == 2) {
                 holder.tipo.setText("Cliente");
             }
-
-
-            //Cuando presionamos en un item...
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                }
-            });
         }
-
-        //Devuelve el numero de mascotas
         @Override
         public int getItemCount() {
             return duenos.size();
         }
     }
     private class ObtenerClientesTask extends AsyncTask<Void, Void, ArrayList<Dueno>> {
-        //Creamos el array donde almacenaremos todos los datos de las mascotas
+        //Creamos el array donde almacenaremos todos los datos de las cliente
         ArrayList<Dueno> duenosList = new ArrayList<>();
-
         @Override
         protected ArrayList<Dueno> doInBackground(Void... voids) {
             //Ponemos la dirección del .php
@@ -182,12 +163,11 @@ public class ClienteMascota extends AppCompatActivity {
                 while ((linea = lector.readLine()) != null) {
                     respuesta.append(linea);
                 }
-
                 //Convertimos los datos recibidos en un Document
                 Document document = convertirStringToXMLDocument(respuesta.toString());
                 //Obtenemos los elementos de cada mascota
                 NodeList listaMascotas = document.getElementsByTagName("cliente");
-                //Con este bucle conseguimos los datos de cada mascota
+                //Con este bucle conseguimos los datos de cada cliente
                 for (int i = 0; i < listaMascotas.getLength(); i++) {
                     Element element = (Element) listaMascotas.item(i);
                     int idCliente = Integer.parseInt(element.getElementsByTagName("idCliente").item(0).getTextContent());
@@ -197,11 +177,10 @@ public class ClienteMascota extends AppCompatActivity {
                     String nombre = element.getElementsByTagName("nombre").item(0).getTextContent();
                     String telefono = element.getElementsByTagName("telefono").item(0).getTextContent();
                     String dir = element.getElementsByTagName("direccion").item(0).getTextContent();
-                    //Creamos un objeto Mascota con los datos obtenidos
+                    //Creamos un objeto Cliente con los datos obtenidos
                     Dueno m = new Dueno(idCliente, tipo, usuario, dni, nombre, nombre, telefono, dir);
                     duenosList.add(m);
                 }
-
                 //Cerramos la conexión
                 entrada.close();
                 conexion.disconnect();
